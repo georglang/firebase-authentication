c323c9dc44a5a8bf3707a31a778420bea6820543
Georg Lang <georglang0788@gmx.net>
2022-02-02 06:27:28 +0100

    authentication and authorization

---
+++ functions/src/auth/authorized.ts
@@ -0,0 +1,19 @@
+import { environment } from '../../environment';
+import { Request, Response } from 'express';
+
+export function isAuthorized(opts: { hasRole: Array<'admin' | 'user'>; allowSameUser?: boolean }) {
+  return (req: Request, res: Response, next: any) => {
+    const { role, email, uid } = res.locals;
+    const { id } = req.params;
+
+    if (email === environment.authEmail) return next();
+
+    if (opts.allowSameUser && id && uid === id) return next();
+
+    if (!role) return res.status(403).send();
+
+    if (opts.hasRole.includes(role)) return next();
+
+    return res.status(403).send();
+  };
+}
